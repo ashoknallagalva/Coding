dff3a67987975d49524eddcc774ae0fb333604c1
Ashok <nallagalva_a@worksap.co.jp>
2017-04-12 18:52:05 +0800

    added roads_and_libraries file, code is not complete in this problem

---
+++ hackerank/practice/roads_and_libraries.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<list>
+#include<pair>
+
+use namespace std;
+
+int main(){
+	
+	int q;
+	cin>>q;
+	while(q--){
+		int n,m,clib,croad;
+		cin>>n>>m>>clib>>croad;
+		vector<int> comps;
+		vector<bool> visited(n);
+		vector<list<int> > graph(n);
+		queue<int> q;
+		
+		for(int i=0;i<m;i++){
+			int u,v;
+			
+
+		}
+	}
+}
